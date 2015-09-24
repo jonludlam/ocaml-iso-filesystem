c98e31dbcb81a9a5997e5c15b59a0d40bc093fdb
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-09-25 00:07:52 +0100

    Only make the test iso if mkisofs is installed
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- lib_test/mkiso.sh
+++ lib_test/mkiso.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir test_iso
-echo "hello, world!" > test_iso/hello.txt
-mkisofs -o test.iso -R test_iso
+if type "mkisofs" > /dev/null; then
+  mkdir test_iso
+  echo "hello, world!" > test_iso/hello.txt
+  mkisofs -o test.iso -R test_iso
+else
+  curl http://www.recoil.org/~jon/ocaml-iso9660-test.iso -o test.iso
+fi
 
 
