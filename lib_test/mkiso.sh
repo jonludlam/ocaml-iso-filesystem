e60e8c81309dc8af171ab459e20d07365d7d5425
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-10-04 21:57:55 +0100

    Add a file to the test iso with an old timestamp
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- lib_test/mkiso.sh
+++ lib_test/mkiso.sh
@@ -3,6 +3,8 @@
 if type "mkisofs" > /dev/null; then
   mkdir test_iso
   echo "hello, world!" > test_iso/hello.txt
+  echo "old file" > test_iso/old.txt
+  touch -t 197001010001 test_iso/old.txt
   mkisofs -o test.iso -R test_iso
 else
   curl http://www.recoil.org/~jon/ocaml-iso9660-test.iso -o test.iso
